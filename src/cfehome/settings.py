84c9bca3bd4077c11d71573b3255c2363f5798f2
Shaurya19 <shauryabarkund@gmail.com>
2024-11-14 23:53:30 +0530

    Added support for railway deployment

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure--_4=o6jq7rrbu!eua172qn*e*tw(ooqurzeu*(mn$+em@v04wc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition

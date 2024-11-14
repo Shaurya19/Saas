14bb3e032aaaab8a1c447ac88ff5489c1a57a978
Shaurya19 <shauryabarkund@gmail.com>
2024-11-14 23:29:21 +0530

    Added the visits models

---
+++ src/cfehome/views.py
@@ -0,0 +1,20 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    
+    my_title = "My Page"
+    qs = PageVisit.objects.all()
+    page_visit_count = qs.count()
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_visit_count
+    }
+    html_template = "home.html"
+    PageVisit.objects.create()
+    return render(request, html_template, my_context)

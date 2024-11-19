e0bdcd516a40492cb3e0870f0b52b740e4d4d205
Shaurya19 <shauryabarkund@gmail.com>
2024-11-20 00:23:42 +0530

    Added missing arguments in docker file for railway container build

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CON_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config('DATABASE_URL', cast=str)
+DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL:
     import dj_database_url

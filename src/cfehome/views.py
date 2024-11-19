5d62a46a7698b4b60344bc9abbf57226664c773e
Shaurya19 <shauryabarkund@gmail.com>
2024-11-20 00:14:36 +0530

    Added changes to prepare static files

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -18,3 +18,17 @@ def home_page_view(request, *args, **kwargs):
     html_template = "home.html"
     PageVisit.objects.create()
     return render(request, html_template, my_context)
+
+
+def about_view(request, *args, **kwargs):
+    
+    my_title = "My Page"
+    qs = PageVisit.objects.all()
+    page_visit_count = qs.count()
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_visit_count
+    }
+    html_template = "home.html"
+    PageVisit.objects.create()
+    return render(request, html_template, my_context)
